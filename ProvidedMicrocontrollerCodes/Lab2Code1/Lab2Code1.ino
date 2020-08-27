f7cfbd60d319a46aa7703600a110fa7fdf30c439
Patrick Mayerhofer <patrickmayerhofer@MacBook-Pro-2.local>
2020-08-27 13:18:42 -0700

    Patrick

--- ProvidedMicrocontrollerCodes/Lab2Code1/Lab2Code1.ino
+++ ProvidedMicrocontrollerCodes/Lab2Code1/Lab2Code1.ino
@@ -1,7 +1,7 @@
 
-#define sf 1000 //change this for wanted sampling fq
-//~ #define tc (1000/(sf))     // time constant
-#define tc 1000
+#define sf 300 //change this for wanted sampling fq
+int tc (1000/(sf));     // time constant
+
 unsigned int ADC_Value = 0;    //ADC current value
 unsigned long last_time = 0;
 void setup() {
@@ -12,12 +12,12 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
 
-  if (micros() - last_time >= tc) {
-    last_time = micros();
+  if (millis() - last_time >= tc) {
+    last_time = millis();
     ADC_Value = analogRead(A0);
     
     Serial.print(ADC_Value);
-    Serial.print(',');
+    Serial.print('\t');
     Serial.print(millis());
     Serial.println();
     }
